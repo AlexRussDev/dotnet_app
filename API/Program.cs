abe44c3de9ab7515b3bb3b0f75b40966a7cbd1c4
AlexRussDev <russakov.aleksandr@gmail.com>
2022-06-13 13:28:49 +0300

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,53 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<StoreContext>(opt => {
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+ILogger logger = new LoggerFactory().CreateLogger<Program>();
+
+var app = builder.Build();
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+
+try {
+  context.Database.Migrate();
+  DbInitializer.Initialize(context);
+} 
+catch (Exception ex){
+  logger.LogError(ex, "Problem migrating data");
+}
+finally 
+{
+  scope.Dispose();
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
